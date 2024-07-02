f48a0950cdb90d2cd762eb497d7e8071f2af4ca2
Polina Migel <p.ooooh@yandex.ru>
2024-07-02 16:55:08 +0300

    tests: fix importing of Application

--- tests/test_application.py
+++ tests/test_application.py
@@ -7,7 +7,7 @@ import re
 
 from telegram.ext import CommandHandler
 
-from hammett.core import Application
+from hammett.core.application import Application
 from hammett.core.button import Button
 from hammett.core.constants import DEFAULT_STATE, SourcesTypes
 from hammett.core.exceptions import TokenIsNotSpecified
