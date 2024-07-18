374e0d03b77a5697a5f709bca8872e9978b2897f
Polina Migel <p.ooooh@yandex.ru>
2024-07-18 18:03:15 +0300

    tests: add extra tests in test_persistence.py

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -3,11 +3,14 @@
 # ruff: noqa: ANN201, SLF001
 
 import json
+from pathlib import Path
 
 from fakeredis import FakeAsyncRedis
 
-from hammett.core.persistence import RedisPersistence
+from hammett.core.exceptions import ImproperlyConfigured
+from hammett.core.persistence import RedisPersistence, _Encoder
 from hammett.test.base import BaseTestCase
+from hammett.test.utils import override_settings
 
 _BOT_NAME = 'test'
 
@@ -16,6 +19,8 @@ _CHAT_ID = 123456789
 _DATA = {'key1': 'value1', 'key2': 'value2'}
 
 _NEW_STATE = 'new_state'
+
+_TEST_KEY = 'test_key'
 
 _USER_ID = 123456789
 
@@ -54,6 +59,16 @@ class PersistenceTests(BaseTestCase):
         })
         self.assertDictEqual(decoded_data, {_USER_ID: _DATA})
 
+    async def test_dropping_of_empty_chat_data(self):
+        """Test dropping of the empty chat_data."""
+        await self.persistence.drop_chat_data(_CHAT_ID)
+        self.assertIsNone(self.persistence.user_data)
+
+    async def test_dropping_of_empty_user_data(self):
+        """Test dropping of the empty user_data."""
+        await self.persistence.drop_user_data(_USER_ID)
+        self.assertIsNone(self.persistence.user_data)
+
     async def test_getting_and_setting_of_bot_data(self):
         """Test getting and setting of the bot_data."""
         bot_data = await self.persistence.get_bot_data()
@@ -83,6 +98,13 @@ class PersistenceTests(BaseTestCase):
         updated_conversations = await self.persistence.get_conversations(_BOT_NAME)
         self.assertEqual(updated_conversations, {(_CHAT_ID, _USER_ID): _NEW_STATE})
 
+    async def test_getting_data_from_database_directly(self):
+        """Test getting a data from a database directly."""
+        await self.persistence._set_data(_TEST_KEY, _DATA)
+
+        data = await self.persistence._get_data(_TEST_KEY)
+        self.assertEqual(data, _DATA)
+
     async def test_getting_setting_and_dropping_of_chat_data(self):
         """Test getting, setting and dropping of the chat_data."""
         chat_data = await self.persistence.get_chat_data()
@@ -109,6 +131,14 @@ class PersistenceTests(BaseTestCase):
         dropped_user_data = await self.persistence.get_user_data()
         self.assertEqual(dropped_user_data, {})
 
+    async def test_handling_of_error_when_getting_data(self):
+        """Test the case when an error is raised when getting data."""
+        encoded_data = json.dumps(_DATA) + '1'
+        await self.persistence.redis_cli.set(_TEST_KEY, encoded_data)
+
+        decoded_data = await self.persistence._get_data(_TEST_KEY)
+        self.assertIsNone(decoded_data)
+
     async def test_hgetall_by_chunks_method(self):
         """Test a hgetall_by_chunks persistence method."""
         data = {_USER_ID: _DATA}
@@ -117,6 +147,22 @@ class PersistenceTests(BaseTestCase):
         encoded_data = await self.persistence._hgetall_by_chunks('test_key')
         decoded_data = self.persistence._decode_data(encoded_data)
         self.assertEqual(decoded_data, data)
+
+    @override_settings(REDIS_PERSISTENCE={})
+    def test_initialization_without_db_specified(self):
+        """Test an initialization without specifying DB attribute."""
+        with self.assertRaises(ImproperlyConfigured):
+            RedisPersistence()
+
+    def test_passing_path_type_object_to_custom_encoder(self):
+        """Test passing a Path type object to a custom encoder."""
+        path = json.dumps(Path('/path/to/hammett'), cls=_Encoder)
+        self.assertEqual('/path/to/hammett', json.loads(path))
+
+    def test_passing_unsupported_type_to_custom_encoder(self):
+        """Test passing an unsupported type to a custom encoder."""
+        with self.assertRaises(TypeError):
+            json.dumps(b'test', cls=_Encoder)
 
     async def test_storing_of_data_after_flushing_database(self):
         """Test storing of the data after flushing the database."""
@@ -138,4 +184,34 @@ class PersistenceTests(BaseTestCase):
             },
         })
         self.assertEqual(self.persistence.chat_data, {_CHAT_ID: _DATA})
+        self.assertEqual(self.persistence.user_data, {_USER_ID: _DATA})
+
+    async def test_updating_persistence_data_when_it_is_up_to_date(self):
+        """Test updating chat_data when it is up-to-date."""
+        await self.persistence.update_bot_data(_DATA)
+        await self.persistence.update_bot_data(_DATA)
+        self.assertEqual(self.persistence.bot_data, _DATA)
+
+        await self.persistence.update_callback_data(([], _DATA))
+        await self.persistence.update_callback_data(([], _DATA))
+        self.assertEqual(self.persistence.callback_data, ([], _DATA))
+
+        await self.persistence.update_conversation(
+            _BOT_NAME, (_CHAT_ID, _USER_ID), _NEW_STATE,
+        )
+        await self.persistence.update_conversation(
+            _BOT_NAME, (_CHAT_ID, _USER_ID), _NEW_STATE,
+        )
+        self.assertEqual(self.persistence.conversations, {
+            _BOT_NAME: {
+                (_CHAT_ID, _USER_ID): _NEW_STATE,
+            },
+        })
+
+        await self.persistence.update_chat_data(_CHAT_ID, _DATA)
+        await self.persistence.update_chat_data(_CHAT_ID, _DATA)
+        self.assertEqual(self.persistence.chat_data, {_CHAT_ID: _DATA})
+
+        await self.persistence.update_user_data(_USER_ID, _DATA)
+        await self.persistence.update_user_data(_USER_ID, _DATA)
         self.assertEqual(self.persistence.user_data, {_USER_ID: _DATA})
